15e40dd792fb7596a2cbe7c6c2bbc99da759ce00
Select9th <66799648+Select9th@users.noreply.github.com>
2020-06-11 19:57:24 -0400

    Add files via upload

---
+++ terrariasubnautica-master/Content/Items/Materials/UraniniteCrystal.cs
@@ -0,0 +1,21 @@
+﻿using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace SubnauticMod.Content.Items.Materials{
+    public class UraniniteCrystal : ModItem {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Uraninte");
+            Tooltip.SetDefault("Yes");
+        }
+
+        public override void SetDefaults() {
+            item.width = 16;
+            item.height = 16;
+            item.maxStack = 999;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.useTime = 10;
+            item.useAnimation = 10;
+            item.autoReuse = true;
+        }
+    }
+}
