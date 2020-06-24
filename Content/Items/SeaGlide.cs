c43aaaed81a4b59b0d49cc9243b297b15a858890
Ilham Aryasuta <ilhamaryasuta@gmail.com>
2020-06-24 08:55:54 +0700

    seaglider texture

--- Content/Items/SeaGlide.cs
+++ Content/Items/SeaGlide.cs
@@ -1,9 +1,11 @@
-﻿using Terraria.ID;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace SubnauticMod.Content.Items {
 	public class SeaGlide : ModItem {
-		public override string Texture => "Terraria/Item_" + ItemID.Umbrella;
+		//public override string Texture => "Terraria/Item_" + ItemID.Umbrella;
 
 		public override void SetStaticDefaults() {
 			//DisplayName.SetDefault("TutorialSword"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
@@ -14,11 +16,13 @@ namespace SubnauticMod.Content.Items {
 		public override void SetDefaults() {
 			item.channel = true;
 			item.noMelee = true;
-			item.width = 40;
-			item.height = 40;
+			item.useStyle = ItemUseStyleID.HoldingOut;
+			item.width = 44;
+			item.height = 26;
 			item.value = 1000;
 			item.rare = ItemRarityID.Green;
-			item.holdStyle = ItemHoldStyleID.HoldingUp;
+			item.holdStyle = ItemHoldStyleID.HarpHoldingOut;
+			item.scale = 0.75f;
 		}
 
 		public override void AddRecipes() {
@@ -27,6 +31,14 @@ namespace SubnauticMod.Content.Items {
 			recipe.AddTile(ModContent.TileType<Tiles.Fabricator>());
 			recipe.SetResult(this);
 			recipe.AddRecipe();
+		}
+
+		public override bool CanUseItem(Player player) {
+			return false;
+		}
+
+		public override Vector2? HoldoutOffset() {
+			return Vector2.Zero;
 		}
 
 	}
