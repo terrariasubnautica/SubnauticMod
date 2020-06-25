000cc8bf4869c1001e9a0143de0817e6ea968e88
Ilham Aryasuta <ilhamaryasuta@gmail.com>
2020-06-26 01:28:41 +0700

    add ancient sand

---
+++ Content/Tiles/AncientSand.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SubnauticMod.Content.Tiles {
+	public class AncientSand : ModTile {
+		public override void SetDefaults() {
+			Main.tileSolid[Type] = true;
+			Main.tileBrick[Type] = true;
+			Main.tileMergeDirt[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			Main.tileSand[Type] = true;
+			TileID.Sets.TouchDamageSands[Type] = 15;
+			TileID.Sets.Conversion.Sand[Type] = true; // Allows Clentaminator solutions to convert this tile to their respective Sand tiles.
+			TileID.Sets.ForAdvancedCollision.ForSandshark[Type] = true; // Allows Sandshark enemies to "swim" in this sand.
+			AddMapEntry(new Color(200, 200, 200));
+			drop = ModContent.ItemType<Items.Placeables.AncientSand>();
+		}
+	}
+}
